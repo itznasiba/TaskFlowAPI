ac917df3403fa76e5d1dbc3ddc8a324023171010
itznasiba <zeynallinesibe2006@gmail.com>
2026-04-23 23:14:45 +0400

    Implemented Swagger UI

--- youtubeAPI/Program.cs
+++ youtubeAPI/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using YoutubeAPI.Business.Services;
 using YoutubeAPI.DataAccess.Repository;
 using YoutubeAPI.Business.Profiles;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI; // Add this using directive
+using Swashbuckle.AspNetCore.Swagger;   // Add this using directive
+using Swashbuckle.AspNetCore.SwaggerGen; // Add this using directive
 
 namespace YoutubeAPI
 {
@@ -28,6 +33,12 @@ namespace YoutubeAPI
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddScoped<IUserService, UserService>();
 
+            // Add Swagger services
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "YoutubeAPI", Version = "v1" });
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -41,6 +52,15 @@ namespace YoutubeAPI
             app.UseAuthorization();
 
             app.MapControllers();
+
+            // Use Swagger middleware
+            app.UseSwagger(); // <-- This requires Swashbuckle.AspNetCore NuGet package
+            app.UseSwaggerUI(c =>
+            {
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "YoutubeAPI v1");
+                }
+            });
 
             app.Run();
         }
